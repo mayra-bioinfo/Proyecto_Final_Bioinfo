dc0c45d8b412794253220df214a882795dced05a
celi-micro <cmartinez184@alumnos.uaq.mx>
2025-11-19 00:10:26 -0600

    Update Script_final.R

--- Script_final.R
+++ Script_final.R
@@ -1,7 +1,10 @@
 MHC_padre <- c("B*58:01", "B*13:01")
 MHC_madre <- c("A*23:01", "A*29:02")
 
-
+##Validacion 
+if(length(MHC_padre) !=2  |  length (MHC_madre) !=2) {
+  stop("Cada progenitor debe tener exactamente 2 alelos")
+  }
 
 
 
