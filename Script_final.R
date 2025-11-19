fd1c8fa198f890fdbd717f6b79bf8880f1758c67
César Terrazas <jterrazas15@alumnos.uaq.mx>
2025-11-19 00:17:13 -0600

    Simulacion de los hijos

--- Script_final.R
+++ Script_final.R
@@ -1,10 +1,25 @@
 MHC_padre <- c("B*58:01", "B*13:01")
 MHC_madre <- c("A*23:01", "A*29:02")
 
+### Modelo de prediccion ###
+
+predecir_MHC <- function(MHC_padre, MHC_madre, n_hijos = 1) {
+
 ##Validacion 
 if(length(MHC_padre) !=2  |  length (MHC_madre) !=2) {
   stop("Cada progenitor debe tener exactamente 2 alelos")
-  }
+}
+
+### Simulacion de hijos ###
+hijos <- list ()
+for (i in 1:n_hijos){
+  alelo_padre <- sample(MHC_padre, 1)
+  alelo_madre <- sample(MHC_madre, 1)
+  genotipo <- c(alelo_padre, alelo_madre)
+  hijos[[i]] <- genotipo
+}
+
+}
 
 
 
